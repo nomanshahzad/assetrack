54c8fbe4b2cba3e745dff929490eee1e08ab783f
Noman Shahzad <nouman.shehzad@amroodlabs.com>
2026-05-15 21:56:20 +0500

    added translations for arabic and english

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     session[:locale] = params[:locale] if params[:locale].present?
-    I18n.locale = session[:locale].presence || I18n.default_locale
+    I18n.locale = session[:locale].presence&.to_sym || I18n.default_locale
   end
 end
